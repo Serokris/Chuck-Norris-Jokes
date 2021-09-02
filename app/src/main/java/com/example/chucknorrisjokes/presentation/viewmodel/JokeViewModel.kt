0c06a62e8bcb2a00218ab927519da91d88b0291d
Serokris <Serokris@PC.Dlink>
2021-09-02 21:19:27 +0300

    Added I/O managers for some suspend functions

--- app/src/main/java/com/example/chucknorrisjokes/presentation/viewmodel/JokeViewModel.kt
+++ app/src/main/java/com/example/chucknorrisjokes/presentation/viewmodel/JokeViewModel.kt
@@ -4,10 +4,14 @@ import androidx.lifecycle.ViewModel
 import com.example.chucknorrisjokes.domain.models.response.JokeResponse
 import com.example.chucknorrisjokes.domain.usecases.JokeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class JokeViewModel @Inject constructor(private val jokeUseCase: JokeUseCase) : ViewModel() {
-    suspend fun fetchRandomJoke(): Response<JokeResponse> = jokeUseCase.fetchRandomJoke()
+    suspend fun fetchRandomJoke(): Response<JokeResponse> {
+        return withContext(Dispatchers.IO) { jokeUseCase.fetchRandomJoke() }
+    }
 }
